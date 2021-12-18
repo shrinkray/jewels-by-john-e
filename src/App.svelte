1549a005ba68ef768bd9c991393297adf2d07e36
Greg Miller <hello@gregmiller.io>
2021-12-18 09:58:34 -0500

    feat: Single page app
    Add overlapping panel & footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
-	import ModeSwitcher from './ModeSwitcher.svelte';
 	import Tailwindcss from './Tailwindcss.svelte';
+    import OverlappingPanel from "./OverlappingPanel.svelte";
+    import Footer from "./Footer.svelte";
 </script>
 
+<style>
+    .font-sans	{
+        font-family: 'Inter', sans-serif;
+    }
+    .font-serif	{
+        font-family: ui-serif, Georgia, Cambria, "Times New Roman", Times, serif;
+    }
+    .font-mono	{
+        font-family: 'Fira Code', ui-monospace, Consolas, "Liberation Mono", monospace;
+    }
+</style>
+
 <Tailwindcss />
-<main class="p-4 mx-auto text-center max-w-xl">
+
+<svelte:head>
+	<title>Jewels by John E</title>
+	<meta name="robots" content="noindex nofollow" />
+	<html lang="en" />
+</svelte:head>
+
+<main class="p-4 mx-auto text-center max-w-full font-sans">
+    <OverlappingPanel />
+
+    
 
 </main>
+<Footer />
