4f339e60beca734ea9a6432b35282792f059fe02
Greg Miller <hello@gregmiller.io>
2021-12-20 16:00:51 -0500

    feat: Minor adjustments to overlap image

--- src/OverlappingPanel.svelte
+++ src/OverlappingPanel.svelte
@@ -1,7 +1,7 @@
 
 <div class="relative py-16 bg-white">
   <div class="hidden absolute top-0 inset-x-0 h-1/2 bg-gray-50 lg:block" aria-hidden="true"></div>
-  <div class="max-w-7xl mx-auto bg-indigo-600 lg:bg-transparent lg:px-8">
+  <div class="max-w-7xl mx-auto bg-fuchsia-700 lg:bg-transparent lg:px-8 rounded-3xl">
     <div class="lg:grid lg:grid-cols-12">
       <div class="relative z-10 lg:col-start-1 lg:row-start-1 lg:col-span-4 lg:py-16 lg:bg-transparent">
         <div class="absolute inset-x-0 h-1/2 bg-gray-50 lg:hidden" aria-hidden="true"></div>
@@ -12,15 +12,17 @@
         </div>
       </div>
 
-      <div class="relative bg-indigo-600 lg:col-start-3 lg:row-start-1 lg:col-span-10 lg:rounded-3xl lg:grid lg:grid-cols-10 lg:items-center">
-        <div class="hidden absolute inset-0 overflow-hidden rounded-3xl lg:block" aria-hidden="true">
-          <img class='h-full w-full object-cover' src = 'https://images.unsplash.com/photo-1597177586824-33bda0c29325?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80' alt='precious stones' >
-          <div class="absolute inset-0 bg-fuchsia-700 mix-blend-multiply"></div>
+      <div class="relative bg-fuchsia-700 lg:col-start-3 lg:row-start-1 lg:col-span-10 rounded-3xl lg:grid lg:grid-cols-10 lg:items-center">
+        <div class="hidden absolute inset-0 overflow-hidden rounded-3xl sm:block" aria-hidden="true">
+          <img class='h-full w-full object-cover sm:blur-sm md:blur-none' src = 'https://images.unsplash.com/photo-1597177586824-33bda0c29325?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80' alt='precious stones' >
+          <div class="absolute inset-0 bg-fuchsia-600 mix-blend-multiply"></div>
         </div>
         <div class="relative max-w-md mx-auto py-12 px-4 space-y-6 sm:max-w-3xl sm:py-16 sm:px-6 lg:max-w-none lg:p-0 lg:col-start-4 lg:col-span-6">
           <h1 class="text-3xl font-extrabold text-white" id="join-heading">Jewels by John E</h1>
+          
           <p class="text-lg text-white">Natural Stones and Swarovski Crystals. <br>Vintage and Costume Jewelry</p>
-          <a class="block w-auto inline-flex items-center py-3 px-5 space-around bg-white border border-transparent rounded-md shadow-md text-base font-medium text-indigo-700 hover:bg-gray-50" href="#"><svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="gift" class="w-6 h-6 fill-fuchsia-700" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g class="fa-duotone-group"><path class="fill-fuchsia-500" fill="" d="M152 176H224V256H32C14.33 256 0 241.7 0 224V160C0 142.3 14.33 128 32 128H73.6C88.16 156.5 117.8 176 152 176zM480 256H288V176H360C394.2 176 423.8 156.5 438.4 128H480C497.7 128 512 142.3 512 160V224C512 241.7 497.7 256 480 256zM32 288H224V512H80C53.49 512 32 490.5 32 464V288zM288 512V288H480V464C480 490.5 458.5 512 432 512H288z"></path><path class="fill-fuchsia-400" fill="" d="M280.1 44.45C296.3 16.91 325.9 0 357.8 0H360C408.6 0 448 39.4 448 88C448 136.6 408.6 176 360 176H288V256H224V176H152C103.4 176 64 136.6 64 88C64 39.4 103.4 0 152 0H154.2C186.1 0 215.7 16.91 231.9 44.45L256 85.46L280.1 44.45zM190.5 68.78C182.9 55.91 169.1 48 154.2 48H152C129.9 48 112 65.91 112 88C112 110.1 129.9 128 152 128H225.3L190.5 68.78zM286.7 128H360C382.1 128 400 110.1 400 88C400 65.91 382.1 48 360 48H357.8C342.9 48 329.1 55.91 321.5 68.78L286.7 128zM224 512V288H288V512H224z"></path></g></svg><span class='ml-2 text-xl'>Buy Unique Gifts</span></a>
+          
+          <a class="block w-auto inline-flex items-center py-3 px-5 space-around bg-white border border-transparent rounded-md shadow-md text-base font-medium text-fuchsia-700 hover:bg-gray-50" href="#"><svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="gift" class="w-6 h-6 fill-fuchsia-700" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g class="fa-duotone-group"><path class="fill-fuchsia-500" fill="" d="M152 176H224V256H32C14.33 256 0 241.7 0 224V160C0 142.3 14.33 128 32 128H73.6C88.16 156.5 117.8 176 152 176zM480 256H288V176H360C394.2 176 423.8 156.5 438.4 128H480C497.7 128 512 142.3 512 160V224C512 241.7 497.7 256 480 256zM32 288H224V512H80C53.49 512 32 490.5 32 464V288zM288 512V288H480V464C480 490.5 458.5 512 432 512H288z"></path><path class="fill-fuchsia-400" fill="" d="M280.1 44.45C296.3 16.91 325.9 0 357.8 0H360C408.6 0 448 39.4 448 88C448 136.6 408.6 176 360 176H288V256H224V176H152C103.4 176 64 136.6 64 88C64 39.4 103.4 0 152 0H154.2C186.1 0 215.7 16.91 231.9 44.45L256 85.46L280.1 44.45zM190.5 68.78C182.9 55.91 169.1 48 154.2 48H152C129.9 48 112 65.91 112 88C112 110.1 129.9 128 152 128H225.3L190.5 68.78zM286.7 128H360C382.1 128 400 110.1 400 88C400 65.91 382.1 48 360 48H357.8C342.9 48 329.1 55.91 321.5 68.78L286.7 128zM224 512V288H288V512H224z"></path></g></svg><span class='ml-2 text-xl'>Buy Unique Gifts</span></a>
         </div>
       </div>
     </div>
